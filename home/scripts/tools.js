52c74a982b337e3d22cd4c99bf2758210b6d13ec
Seth-SH <62153326+Kash-001@users.noreply.github.com>
2024-03-26 23:25:48 +0100

    Update tools.js

--- home/scripts/tools.js
+++ home/scripts/tools.js
@@ -86,12 +86,12 @@ function scanAllHosts(forceRescan) {
     const ipLinks = document.querySelectorAll("#column-list ul li a.button");
   
     ipLinks.forEach(function (ipLink) {
-      const ipAddressMatch = ipLink.textContent.trim().match(/\b(?:\d{1,3}\.){3}\d{1,3}\b/); // Match IP address pattern
-      const ipAddress = ipAddressMatch ? ipAddressMatch[0] : null; // Extract IP address from the match
+      const ipAddressMatch = ipLink.textContent.trim().match(/\b(?:\d{1,3}\.){3}\d{1,3}\b/);
+      const ipAddress = ipAddressMatch ? ipAddressMatch[0] : null;
   
       if (ipAddress && !isPopupOpen && (forceRescan || ipLink.dataset.scanStatus !== "scanned")) {
         const url = "http://" + ipAddress + ":53597/health";
-        console.log("URL:", url); // Log the constructed URL for debugging
+        console.log("URL:", url);
         fetch(url)
           .then(function (response) {
             return response.json();
@@ -124,10 +124,77 @@ function scanAllHosts(forceRescan) {
 function initializeIPS() {
     const ipLinks = document.querySelectorAll("#column-list ul li a.button");
     ips = Array.from(ipLinks).map(ipLink => {
-        const ipAddressMatch = ipLink.textContent.trim().match(/\b(?:\d{1,3}\.){3}\d{1,3}\b/); // Match IP address pattern
-        const ipAddress = ipAddressMatch ? ipAddressMatch[0] : null; // Extract IP address from the match
+        const ipAddressMatch = ipLink.textContent.trim().match(/\b(?:\d{1,3}\.){3}\d{1,3}\b/);
+        const ipAddress = ipAddressMatch ? ipAddressMatch[0] : null;
         return ipAddress;
     });
+}
+
+function displayLastScanData(event) {
+  event.preventDefault();
+  const selectedIPText = event.target.textContent.trim();
+
+  let selectedIP = "";
+  for (const ip of ips) {
+      if (selectedIPText.includes(ip)) {
+          selectedIP = ip;
+          break;
+      }
+  }
+
+  if (selectedIP) {
+      const url = `http://${selectedIP}:53597/lastscan`;
+
+      fetch(url)
+          .then(response => response.json())
+          .then(data => {
+              const contentDiv = document.getElementById("content");
+
+              const scanResultElements = contentDiv.querySelectorAll('.scan-result');
+              scanResultElements.forEach(element => {
+                  element.remove();
+              });
+
+              const ipHeader = document.createElement("h2");
+              ipHeader.classList.add('scan-result');
+              const lastScanTime = document.createElement("p");
+              lastScanTime.classList.add('scan-result');
+              const totalOnlineMachines = document.createElement("p");
+              totalOnlineMachines.classList.add('scan-result');
+              const scanResultsList = document.createElement("ul");
+              scanResultsList.classList.add('scan-result');
+              ipHeader.textContent = `${selectedIP} / ${data.hostname}`;
+              lastScanTime.textContent = `Last scan date: ${data.last_scan_time}`;
+              totalOnlineMachines.textContent = `Total online machines: ${data.clients}`;
+
+              for (const [ip, ports] of Object.entries(data.scan_results)) {
+                  const listItem = document.createElement("li");
+                  listItem.textContent = `${ip}:`;
+
+                  const portsList = document.createElement("ul");
+                  const portsArray = ports.split(" ");
+
+                  portsArray.forEach(port => {
+                      const portItem = document.createElement("li");
+                      portItem.textContent = port;
+                      portsList.appendChild(portItem);
+                  });
+
+                  listItem.appendChild(portsList);
+                  scanResultsList.appendChild(listItem);
+              }
+
+              contentDiv.appendChild(ipHeader);
+              contentDiv.appendChild(lastScanTime);
+              contentDiv.appendChild(totalOnlineMachines);
+              contentDiv.appendChild(scanResultsList);
+          })
+          .catch(error => {
+              console.error(`Error during the API request for IP ${selectedIP}:`, error);
+          });
+  } else {
+      console.error("Could not find a matching IP address in the 'ips' array.");
+  }
 }
 
 document.getElementById("toolsButton").addEventListener("click", openPopup);
@@ -136,6 +203,11 @@ setInterval(function () {
     scanAllHosts(false);
     populateDropdown();
 }, 3000);
+
+const ipLinks = document.querySelectorAll("#column-list ul li a.button");
+ipLinks.forEach(ipLink => {
+    ipLink.addEventListener("click", displayLastScanData);
+});
 
 window.addEventListener("load", function() {
     initializeIPS();
