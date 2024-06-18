ac2a87aef5ce0a6de1e8c5440daa4835c17dea22
Seth-SH <62153326+Kash-001@users.noreply.github.com>
2024-06-18 19:51:32 +0200

    Update home.php

--- home/home.php
+++ home/home.php
@@ -11,7 +11,7 @@ if (!isset($_SESSION["username"])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/styles.css">
     <link rel="stylesheet" href="css/tools.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@400;700&display=swap">
     <link rel="icon" type="image/x-icon" href="../deps/media/seahawks.ico">
